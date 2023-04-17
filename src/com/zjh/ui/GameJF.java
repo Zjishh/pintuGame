d52986b2fd641ead380855d25aa0d26e482fbc56
38580 <385802188@qq.com>
2023-04-17 21:00:19 +0800

    游戏主体完成

--- src/com/zjh/ui/GameJF.java
+++ src/com/zjh/ui/GameJF.java
@@ -2,8 +2,7 @@ package com.zjh.ui;
 
 import javax.swing.*;
 import javax.swing.border.BevelBorder;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.event.*;
 import java.util.Random;
 
 /****************************
@@ -18,8 +17,10 @@ public class GameJF extends JFrame {
     int[] imgnum = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
     int[][] imgnum2 = new int[4][4];
     int x0 = 0, y0 = 0;
-    int temp1[][];
-    String path = "image\\girl\\girl6\\";//**
+    int win[][] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 0}};
+    String path = "image\\girl\\girl1\\";//**
+    int step = 0;
+
     public GameJF() {
         init();
 
@@ -47,12 +48,13 @@ public class GameJF extends JFrame {
 
         for (int i = 0; i < imgnum.length; i++) {
             if (imgnum[i] == 0) {
-                System.out.println("0索引的下标是" + x0 + "," + y0);
+
                 x0 = i / 4;
                 y0 = i % 4;
-            } else {
-                imgnum2[i / 4][i % 4] = imgnum[i];
+                System.out.println("0索引的下标是" + x0 + "," + y0);
             }
+                imgnum2[i / 4][i % 4] = imgnum[i];
+
 
         }
     }
@@ -60,6 +62,16 @@ public class GameJF extends JFrame {
     private void initImage() {
         //清空原本的图片
         this.getContentPane().removeAll();
+
+        if (Victory()) {
+            JLabel winimg = new JLabel(new ImageIcon("A:\\JetBrains\\ideaProjects\\pintu\\image\\win.png"));
+            winimg.setBounds(203, 283, 197, 73);
+            this.getContentPane().add(winimg);
+        }
+
+        JLabel label1 = new JLabel("步数："+step);
+        label1.setBounds(50,30,100,20);
+        this.getContentPane().add(label1);
 
         JLabel backimg = new JLabel(new ImageIcon("image\\background.png"));
 
@@ -96,10 +108,52 @@ public class GameJF extends JFrame {
 
         //创建选项下面的条目对象
         JMenuItem replayItem = new JMenuItem("重新游戏");
+        replayItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                step = 0;
+                //d打乱数据
+                initData();
+
+                initImage();
+
+            }
+        });
         JMenuItem reLoginItem = new JMenuItem("重新登录");
+        reLoginItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                GameJF.this.setVisible(false);
+                new LoginJF();
+            }
+        });
         JMenuItem closeItem = new JMenuItem("关闭游戏");
+        closeItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
 
         JMenuItem accountItem = new JMenuItem("公众号");
+        accountItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //弹窗
+                JDialog jDialog = new JDialog();
+                JLabel label = new JLabel(new ImageIcon("image\\about.png"));
+                label.setBounds(0,0,258,258);
+                jDialog.getContentPane().add(label);
+                jDialog.setSize(344,344);
+                //置顶
+                jDialog.setAlwaysOnTop(true);
+                //弹窗居中
+                jDialog.setLocationRelativeTo(null);
+                //不关闭无法惊醒接下来的操作
+                jDialog.setModal(true);
+                jDialog.setVisible(true);
+            }
+        });
 
         // //将每一个选项下面的条目添加到选项当中
         functionJMenu.add(replayItem);
@@ -136,10 +190,10 @@ public class GameJF extends JFrame {
             //按下不送
             public void keyPressed(KeyEvent e) {
                 int keyCode = e.getKeyCode();
-                if (keyCode == 65){
+                if (keyCode == 65) {
                     //清空原本的图片
                     GameJF.this.getContentPane().removeAll();
-                    JLabel backALL = new JLabel(new ImageIcon(path+"all.jpg"));
+                    JLabel backALL = new JLabel(new ImageIcon(path + "all.jpg"));
 
                     backALL.setBounds(83, 134, 420, 420);
                     GameJF.this.getContentPane().add(backALL);
@@ -157,47 +211,60 @@ public class GameJF extends JFrame {
             }
 
             @Override
+            //松开按键的时候
             public void keyReleased(KeyEvent e) {
                 //对上下左右判断
                 //左上右下37，38，39，40
                 int keyCode = e.getKeyCode();
-                if (keyCode == 65){
+                if (keyCode == 65) {
+                    initImage();
+                } else if (keyCode == 87) {
+                    imgnum2 = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 0}};
+                    x0 = 3;
+                    y0 = 3;
                     initImage();
                 }
+//                else if(Victory()){
+//                    return;
+//                }
                 switch (keyCode) {
                     case 37:
-                        if (y0 == 3){
+                        step++;
+                        if (y0 == 3) {
                             break;
                         }
-                        imgnum2[x0][y0] = imgnum2[x0][y0+1];
-                        imgnum2[x0][y0+1] = 0;
+                        imgnum2[x0][y0] = imgnum2[x0][y0 + 1];
+                        imgnum2[x0][y0 + 1] = 0;
                         y0++;
                         initImage();
                         break;
                     case 38:
-                        if (x0 == 3){
+                        step++;
+                        if (x0 == 3) {
                             break;
                         }
-                        imgnum2[x0][y0] = imgnum2[x0+1][y0];
-                        imgnum2[x0+1][y0] = 0;
+                        imgnum2[x0][y0] = imgnum2[x0 + 1][y0];
+                        imgnum2[x0 + 1][y0] = 0;
                         x0++;
                         initImage();
                         break;
                     case 39:
-                        if (y0 == 0){
+                        step++;
+                        if (y0 == 0) {
                             break;
                         }
-                        imgnum2[x0][y0] = imgnum2[x0][y0-1];
-                        imgnum2[x0][y0-1] = 0;
+                        imgnum2[x0][y0] = imgnum2[x0][y0 - 1];
+                        imgnum2[x0][y0 - 1] = 0;
                         y0--;
                         initImage();
                         break;
                     case 40:
-                        if (x0 == 0){
+                        step++;
+                        if (x0 == 0) {
                             break;
                         }
-                        imgnum2[x0][y0] = imgnum2[x0-1][y0];
-                        imgnum2[x0-1][y0] = 0;
+                        imgnum2[x0][y0] = imgnum2[x0 - 1][y0];
+                        imgnum2[x0 - 1][y0] = 0;
                         x0--;
                         initImage();
                         break;
@@ -210,4 +277,17 @@ public class GameJF extends JFrame {
 
     }
 
+    //判断数组是否相等 是否胜利
+    public boolean Victory() {
+        for (int i = 0; i < imgnum2.length; i++) {
+            for (int j = 0; j < imgnum2[i].length; j++) {
+
+                if (imgnum2[i][j] != win[i][j]) {
+                    return false;
+                }
+            }
+
+        }
+        return true;
+    }
 }

535bce7900400dcd20c052e9522bbe59e1558926
38580 <385802188@qq.com>
2023-04-17 21:47:27 +0800

    4.14—— 完成动态点击登录注册按钮233

--- src/com/zjh/ui/LoginJF.java
+++ src/com/zjh/ui/LoginJF.java
@@ -131,7 +131,7 @@ public class LoginJF extends JFrame   {
 
             @Override
             public void mouseExited(MouseEvent e) {
-                System.out.println(1112);
+                System.out.println(111211);
             }
         });
 
